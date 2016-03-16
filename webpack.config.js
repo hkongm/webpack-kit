56d057ef8219ef5a1e5e78cf6ea0fecc080a1afa
caichao <hkongm@163.com>
2016-03-16 17:56:56 +0800

    extractplugins added. but not work.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require("webpack");
 var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   context: __dirname + "/src",
@@ -17,9 +18,14 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ["style", "css", "postcss", "sass"]
       }
     ]
   },
-  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
+  postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ],
+  plugins: [
+    new ExtractTextPlugin("style.css", {
+      allChunks: true
+    })
+  ]
 };
